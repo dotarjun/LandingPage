578c3e33b654d1bc439e598076240c21c2e7afad
Arjun <arjunsingh8112@gmail.com>
2022-03-24 00:22:08 +0530

    quote section ✅

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navLinks = document.getElementsByClassName('nav-links')[0];
+const notNav = document.getElementsByClassName('not-nav')
 
 console.log(navLinks);
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    notNav.classList.toggle('faded');
 })
